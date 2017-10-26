52f8b3c60a5e908de8f6fa91222e222a4c5bf914
agueye <assane.gueye@gmail.com>
2017-10-26 17:34:06 -0400

    Last changes before run

---
+++ bgpstream/import_bgpstream.c
@@ -0,0 +1,199 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "bgpstream.h"
+
+
+
+
+int main(int argc, char * argv [])
+{
+
+  
+  long start;
+  long end;
+  if(argc < 2)
+    {
+      start=1286705410;
+      end=1286709071;
+    }
+  else
+    {
+      int year=atoi(argv[1]);
+      struct tm t;
+      
+      t.tm_year = year;
+      t.tm_mon =0;         
+      t.tm_mday = 1;       
+      t.tm_hour = 0;
+      t.tm_min = 0;
+      t.tm_sec = 0;
+      t.tm_isdst = -1;     
+      start =(long)mktime(&t);
+
+
+      t.tm_year = year;
+      t.tm_mon =11;         
+      t.tm_mday = 31;       
+      t.tm_hour = 23;
+      t.tm_min = 59;
+      t.tm_sec = 59;
+      t.tm_isdst = -1;     
+      end =(long)mktime(&t);
+    }
+  
+  
+  /* Allocate memory for a bgpstream instance */
+  bgpstream_t *bs = bs = bgpstream_create();
+  if(!bs) {
+    fprintf(stderr, "ERROR: Could not create BGPStream instance\n");
+    return -1;
+  }
+
+  /* Allocate memory for a re-usable bgprecord instance */
+  bgpstream_record_t *bs_record = bgpstream_record_create();
+  if(bs_record == NULL)
+    {
+      fprintf(stderr, "ERROR: Could not create BGPStream record\n");
+      return -1;
+    }
+
+  /* The broker interface is set by default */
+
+  /* Select bgp data from RRC06 and route-views.jinx collectors only */
+  //bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_COLLECTOR, "rrc19");
+  bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_COLLECTOR, "route-views");
+  bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_COLLECTOR, "route-views2");
+  bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_COLLECTOR, "route-views3");
+  bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_COLLECTOR, "route-views4");
+  bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_COLLECTOR, "route-views6");
+  bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_COLLECTOR, "route-views.eqix");
+  bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_COLLECTOR, "route-views.isc");
+  bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_COLLECTOR, "route-views.kixp");
+  bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_COLLECTOR, "route-views.linx");
+  bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_COLLECTOR, "route-views.nwax");
+  bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_COLLECTOR, "route-views.wide");
+  bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_COLLECTOR, "route-views.sydney");
+  bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_COLLECTOR, "route-views.saopaulo");
+  bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_COLLECTOR, "route-views.telxatl");
+
+  //bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_ELEM_IP_VERSION, "4");
+
+  /* Process updates only */
+  bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_RECORD_TYPE, "updates");
+
+  /* Select a time interval to process:
+   * Sun, 10 Oct 2010 10:10:10 GMT -  Sun, 10 Oct 2010 11:11:11 GMT */
+  bgpstream_add_interval_filter(bs,start,end);
+
+  /* Start bgpstream */
+  if(bgpstream_start(bs) < 0) {
+    fprintf(stderr, "ERROR: Could not init BGPStream\n");
+    return -1;
+  }
+
+  int get_next_ret = 0;
+  int elem_counter = 0;
+
+  /* Pointer to a bgpstream elem, memory is borrowed from bgpstream,
+   * use the elem_copy function to own the memory */
+  bgpstream_elem_t *bs_elem = NULL;
+  
+  char buf[4096] = "";
+  char pfx_buf[128] = "";
+  bgpstream_addr_storage_t  *ip_addr_strg=NULL;
+  bgpstream_addr_version_t ver;
+  int ip_version=0;
+  
+
+  /* Read the stream of records */
+  do
+    {
+      /* Get next record */
+      get_next_ret = bgpstream_get_next_record(bs, bs_record);
+      /* Check if there are new records and if they are valid records */
+      if(get_next_ret && bs_record->status == BGPSTREAM_RECORD_STATUS_VALID_RECORD)
+        {
+	  //fprintf(stderr, "********************************** A VALID RECORD............................\n");
+          /* Get next elem in the record */
+          while((bs_elem = bgpstream_record_get_next_elem (bs_record)) != NULL)
+            {
+	      ip_addr_strg=(bgpstream_addr_storage_t *)&bs_elem->peer_address;
+	      ver=ip_addr_strg->version;
+	      ip_version=bgpstream_ipv2number(ver);
+	      //fprintf(stderr,"IP Version = %d\n", ip_version);
+	      if (ip_version==4){
+		if (bgpstream_as_path_snprintf(buf, 4096, bs_elem->aspath) < 4096)
+		  {
+		    if (buf && buf[0]){
+		      //fprintf(stderr, "Very long path.....\n");
+		      //}
+		      //else
+		      //{
+		      bgpstream_pfx_snprintf(pfx_buf, 128, (bgpstream_pfx_t *)&bs_elem->prefix);
+		      //fprintf(stderr, "IP Version:  IPv%d\n",bgpstream_ipv2number((bgpstream_pfx_t *)&bs_elem->prefix->address));
+		      printf("%s %s\n",buf,pfx_buf);
+		      //reverse(buf);
+		      //fprintf(stderr, "%s %s\n",buf,pfx_buf);
+		    }
+		  }
+		//elem_counter++;
+	      }
+	    }
+        }
+    }
+  while(get_next_ret > 0);
+
+  /* Print the number of elems */
+  // printf("\tRead %d elems\n", elem_counter);
+
+  /* De-allocate memory for the bgpstream record */
+  bgpstream_record_destroy(bs_record);
+
+  /* De-allocate memory for the bgpstream */
+  bgpstream_destroy(bs);
+
+  return 0;
+}
+
+/*
+
+ 
+int string_length(char *pointer)
+{
+   int c = 0;
+ 
+   while( *(pointer + c) != '\0' )
+      c++;
+ 
+   return c;
+}
+
+
+
+void reverse(char *string) 
+{
+   int length, c;
+   char *begin, *end, temp;
+ 
+   length = string_length(string);
+   begin  = string;
+   end    = string;
+ 
+   for (c = 0; c < length - 1; c++)
+      end++;
+ 
+   for (c = 0; c < length/2; c++)
+   {        
+      temp   = *end;
+      *end   = *begin;
+      *begin = temp;
+ 
+      begin++;
+      end--;
+   }
+}
+
+
+
+*/
